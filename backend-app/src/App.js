6f7dbbf3462eeb5c6eef5329a9ac12b2195844f6
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2023-10-07 15:00:30 -0400

    connected with firebase to read and write data

--- backend-app/src/App.js
+++ backend-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import AddMovie from "./components/AddMovie";
@@ -7,30 +7,74 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   let content = <p>Found no movies.</p>;
 
-  function fetchMovieHandler() {
+  const fetchMovieHandler = useCallback(() => {
     setLoading(true);
+    setError(null);
 
-    fetch("https://swapi.dev/api/films/")
+    //It will create a new node in firebase database with a name movies.json
+    fetch("https://reactiverse-2842e-default-rtdb.firebaseio.com/movie.json")
       .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong while fetching data!");
+        }
         return response.json();
       })
       .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        console.log(transformedMovies);
-        setMovies(transformedMovies);
+        console.log(data);
+        const loadedMovies = [];
+
+        for (const key in data) {
+          loadedMovies.push({
+            id: key,
+            title: data[key].title,
+            openingText: data[key].openingText,
+            releaseDate: data[key].releaseDate,
+          });
+        }
+        // const transformedMovies = loadedMovies.map((movieData) => {
+        //   return {
+        //     id: movieData.episode_id,
+        //     title: movieData.title,
+        //     openingText: movieData.opening_crawl,
+        //     releaseDate: movieData.release_date,
+        //   };
+        // });
+        console.log(loadedMovies);
+        setMovies(loadedMovies);
         setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(error.message);
       });
-  }
+  }, []);
+
+  const addMovieHandler = (movie) => {
+    fetch("https://reactiverse-2842e-default-rtdb.firebaseio.com/movie.json", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      header: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Sending POST request failed!");
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  //By using useEffect here, we want to fetch data whenever component loads.
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [fetchMovieHandler]);
 
   if (loading) {
     content = <p>Loading...</p>;
@@ -38,11 +82,14 @@ function App() {
     content = <MoviesList movies={movies} />;
   }
 
+  if (error) {
+    content = <p>{error}</p>;
+  }
   return (
     <React.Fragment>
       <section>
         {/* addMovie prop */}
-        <AddMovie />
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
         {/* fetchAPI */}
