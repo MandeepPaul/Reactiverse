00fa7768941e961164d59836a79a1248c327f089
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2023-10-15 14:25:01 -0400

    added custom hook to fetch cart data

--- redux/src/components/Cart/CartButton.js
+++ redux/src/components/Cart/CartButton.js
@@ -4,6 +4,7 @@ import classes from "./CartButton.module.css";
 import { cartActions } from "../../store/cartSlice";
 const CartButton = () => {
   const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+  const showCart = useSelector((state) => state.cart.showCart);
   const dispatch = useDispatch();
 
   const showCartHandler = () => {
@@ -11,7 +12,7 @@ const CartButton = () => {
   };
   return (
     <button className={classes.button} onClick={showCartHandler}>
-      <span>My Cart</span>
+      <span>{showCart ? "Hide Cart" : "See Cart"}</span>
       <span className={classes.badge}>{totalQuantity}</span>
     </button>
   );
--- redux/src/components/Shop/Products.js
+++ redux/src/components/Shop/Products.js
@@ -1,57 +1,51 @@
+import { useState, useEffect, useCallback } from "react";
+
+import useFetch from "../../hooks/use-fetch";
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
 
-const DUMMY_ITEMS = [
-  {
-    id: "m1",
-    title: "Veg Burger",
-    price: "11.99",
-    description:
-      "Fried potato patty in a bun with lettuce, onion, Cucumber, tomato and house made sauces.",
-  },
-  {
-    id: "m2",
-    title: "Spring Rolls",
-    price: "8.99",
-    description:
-      "Noodles and Vegetables wrapped in a phyllo sheet, deep fried come on a bed of crispy noodles salad.    ",
-  },
-  {
-    id: "m3",
-    title: "Dahi Poori",
-    price: "7.99",
-    description:
-      "Round hollow poori fried and filled with a mixture of Sweet Yogurt, Sev, Tamarind sauce, Potatoes, Chillies, Chat Masala, and onions.",
-  },
-  {
-    id: "m4",
-    title: "Steamed Paneer Momos",
-    price: "11.99",
-    description:
-      "8 Pcs of Steam Cooked Dumplings made with Paneer Stuffing and Wrapped in a house-made dough.",
-  },
-  {
-    id: "m5",
-    title: "White Sauce Pasta",
-    price: "11.99",
-    description: "Boiled Pasta Cooked with Vegetables in Rich Creamy Sauce.",
-  },
-];
-
 const Products = () => {
+  const [productsItems, setProductItems] = useState(null);
+  const { result, error, fetchRequest } = useFetch(
+    "https://reactiverse-2842e-default-rtdb.firebaseio.com/AromaOfIndia.json"
+  );
+
+  const fetchData = async () => {
+    try {
+      await fetchRequest();
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (error) return console.log(error);
+
+    setProductItems(result);
+  }, [result, error]);
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      {DUMMY_ITEMS.map((item) => (
-        <ul key={item.id}>
-          <ProductItem
-            id={item.id}
-            title={item.title}
-            price={item.price}
-            description={item.description}
-          />
+      {productsItems === null ? (
+        <span>Loading...</span>
+      ) : (
+        <ul>
+          {productsItems.map((item) => (
+            <ProductItem
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              price={item.price}
+              description={item.description}
+            />
+          ))}
         </ul>
-      ))}
+      )}
     </section>
   );
 };
